ed18cf2dbd5c5b2aa7f61b25ad792772f01aec37
wqk <ccc179@gmail.com>
2019-04-10 20:46:07 +0800

    1,add errors 2,add keys 3,add User() models 4,add User login(submit_code) API and submit_phone API 5,add def YZX sms

--- user/api.py
+++ user/api.py
@@ -2,34 +2,58 @@ from django.shortcuts import render
 from lib import sms
 from lib.http import render_json
 from common import errors
+from django.core.cache import cache
+from common import keys
+from user.models import User
 
 
 # Create your views here.
 def submit_phone(request):
     """获取短信验证码"""
     if not request.method == "POST":
+        '''每次都可以去lib里面的http调用render_json帮助返回json 
+            这个函数要传一个msg和一个状态码code
+        '''
         return render_json("request error",code=errors.REQUEST_ERROR)
     else:
+        # 从post请求里面拿到手机号,将手机号交给send_sms函数去发短信.
         phone = request.POST.get('phone')
         result, msg = sms.send_sms(phone)
+        print(result)
         return render_json(msg)
 
 
 def submit_vcode(request):
     """通过验证码登录注册"""
-    pass
+    if not request.method == "POST":
+        return render_json("request error",code=errors.REQUEST_ERROR)
+    # 取得手机发的验证码/手机号
+    phone = request.POST.get('phone')
+    vcode = request.POST.get('vcode')
+    # 取得缓存中的验证码
+    cache_vcode = cache.get(keys.VCODE_KEY.format(phone))
+
+    #对比验证码是否一致
+    if vcode == cache_vcode:
+        users,_ = User.objects.get_or_create(phonenum=phone)
+        request.session['uid'] = users.id
+        return render_json(users.to_string())
+    else:
+        return render_json('verify code error',errors.VCODE_ERROR)
+
+
 
 
 def get_profile(request):
-    """创建手机号"""
+    """得到轮廓"""
     pass
 
 
 def set_profile(request):
-    """创建手机号"""
+    """设置轮廓"""
     pass
 
 
 def upload_avatar(request):
-    """创建手机号"""
+    """上传头像"""
     pass
